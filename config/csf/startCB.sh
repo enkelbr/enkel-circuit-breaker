ee2190ccc29b33b0d0c2c696727f3daa13eebf5d
Cesar Maia <cesar.maia@enkel.com.br>
2019-06-02 16:57:29 -0300

    Improve shell script Comment out amadeus config

--- config/csf/startCB.sh
+++ config/csf/startCB.sh
@@ -1,17 +1,59 @@
-javaBin=""
-workingDir=""
-minHeap=""
-maxHeap=""
-maxMetaSize=""
-hystrixConfigFile=""
-externalConfigFile=""
-verboseGC=true
+javaBin="/usr/bin/java"
+workingDir="/Users/cemaia/Downloads/target"
+minHeap="4g"
+maxHeap="4g"
+maxMetaSize="512m"
+hystrixConfigFile="${workingDir}/config/hystrix.properties"
+externalConfigFile="${workingDir}/config/application.yaml"
+verboseGC=false
+timeStamp="$(date +%s)"
+
+function echoError {
+    echo -e "\033[31;1m"$1"\033[0m"
+}
+
+function echoWarning {
+    echo -e "\033[33;1m"$1"\033[0m"
+}
+
+function echoSuccess {
+    echo -e "\033[32;1m"$1"\033[0m"
+}
+
+if [ ! -d ${workingDir} ] || [ ! -d ${workingDir}/config ] || [ ! -d ${workingDir}/logs ]
+then
+    RC=0
+    printf "Creating directories: "
+    mkdir -p ${workingDir}
+    RC=$(( $RC + $? ))
+    mkdir -p ${workingDir}/config
+    RC=$(( $RC + $? ))
+    mkdir -p ${workingDir}/logs
+    RC=$(( $RC + $? ))
+    if [ "${RC}" == "0" ]
+    then
+        echoSuccess "DONE"
+    else
+        echoError "Failed"
+        exit 11
+    fi
+fi
+
+if [ -z ${hystrixConfigFile} ] && [ -z ${externalConfigFile} ]
+then
+    echoError "Necessário os arquivos de configuração ${hystrixConfigFile} e ${externalConfigFile}!"
+    exit 10
+fi
 
 if [ "${verboseGC}" == "true" ]
 then
+    echoWarning "Ativando verbose GC..."
     gcInfo="-XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
+fi
 
-${javaBin} \
+printf "Iniciando CB: "
+# set -x
+nohup ${javaBin} \
     -Xms${minHeap} \
     -Xmx${maxHeap} \
     -XX:MaxMetaspaceSize=${maxMetaSize} \
@@ -22,4 +64,12 @@ ${javaBin} \
     -Dserver.port=8080 \
     -Darchaius.configurationSource.additionalUrls=${hystrixConfigFile} \
     -Dspring.config.location=${externalConfigFile} \
-    -jar ${workingDir}/csf-circuit-breaker.jar
+    -jar ${workingDir}/csf-circuit-breaker.jar > ${workingDir}/logs/nohup.${timeStamp}.out
+
+if [ "$?" == "0" ]
+then
+    echoSuccess "DONE"
+else
+    echoError "Failed"
+    cat ${workingDir}/logs/nohup.${timeStamp}.out
+fi
